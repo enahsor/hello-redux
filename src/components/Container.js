162f64328c417a2fb6a46e59fc4c426d14d9d611
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-03-27 13:48:21 -0500

    Updated design

---
+++ src/components/Container.js
@@ -0,0 +1,19 @@
+import React from "react";
+import friends from "../images/social_friends.svg";
+
+class Container extends React.Component{
+
+
+    render(){
+
+        return(
+            <div className="container">
+                <img src={friends} alt="SVG of friends interacting">
+                </img>
+                <h1 className="underline">Redux Test</h1>
+            </div>
+        );
+    }
+}
+
+export default Container;
