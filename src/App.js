134cbeff17f0931627c6bc8d0581e9e237756b4e
Dowen Robinson <drobinson@ad.accelclaims.com>
2020-03-27 14:33:31 -0500

    Added redux store and reducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Container from "./components/Container"
+import Container from "./components/Container";
+import {createStore} from "redux";
+import reducer from "./reducers/";
+
+const initialState = {tech: "React"}
+const store = createStore(reducer, initialState);
 
 class App extends React.Component{
-
+  
   render(){
 
     return(
-      <Container tech="Redux"/>
+      <Container tech={store.getState().tech}/>
     )
   }
 }
